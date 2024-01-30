21890bb9710d58342f5c745a24b9a7e00000ab66
Julien Ramboz <ramboz@adobe.com>
2024-01-30 15:08:17 +0100

    fix: improve production host detection to better cover edge cases

--- src/index.js
+++ src/index.js
@@ -681,7 +681,10 @@ export async function loadLazy(document, options, context) {
   if (window.location.hostname.endsWith('hlx.page')
     || window.location.hostname === ('localhost')
     || (typeof options.isProd === 'function' && !options.isProd())
-    || (options.prodHost && options.prodHost !== window.location.origin)) {
+    || (options.prodHost
+        && options.prodHost !== window.location.host
+        && options.prodHost !== window.location.hostname
+        && options.prodHost !== window.location.origin)) {
     // eslint-disable-next-line import/no-cycle
     const preview = await import('./preview.js');
     preview.default(document, pluginOptions, { ...context, getResolvedAudiences });
