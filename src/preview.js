b8bc1116cb3e561de17d5802b86ce6ef84f9e61b
Julien Ramboz <ramboz@adobe.com>
2024-04-17 10:08:23 -0700

    fix: possible undefined variable

--- src/preview.js
+++ src/preview.js
@@ -99,7 +99,7 @@ function createPopupDialog(header, items = []) {
     list.append(createPopupItem(item));
   });
   const buttons = [...popup.querySelectorAll('.hlx-popup-header-actions .hlx-button a')];
-  actions.forEach((action, index) => {
+  header.actions?.forEach((action, index) => {
     if (action.onclick) {
       buttons[index].addEventListener('click', action.onclick);
     }
